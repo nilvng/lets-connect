bca86f402ad1b3261c27bf94e1f0569d78acbf59
nilvng <nilverspace@gmail.com>
2023-08-27 18:01:55 +1000

    Initial Commit

---
+++ LetsConnect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsConnect
+//
+//  Created by Nil Nguyen on 27/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsConnect/LetsConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsConnectApp.swift
+//  LetsConnect
+//
+//  Created by Nil Nguyen on 27/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct LetsConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
